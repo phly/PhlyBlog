4d5429db025fe240f38f64b374d23fc43df9b6fa
Matthew Weier O'Phinney <matthew@zend.com>
2012-03-28 08:53:09 -0500

    Created Author entity
    - Created author entity; contains id, name, email, and url
    - Created input filter for author
      - created URL validator

---
+++ src/PhlyBlog/Filter/AuthorFilter.php
@@ -0,0 +1,30 @@
+<?php
+namespace PhlyBlog\Filter;
+
+use Zend\Filter\InputFilter;
+
+class AuthorFilter extends InputFilter
+{
+    public function __construct()
+    {
+        $filterRules = array(
+            'id'    => 'string_trim',
+            'name'  => array('string_trim', 'strip_tags', 'html_entities'),
+            'email' => 'string_trim',
+            'url'   => 'string_trim',
+        );
+
+        $validatorRules = array(
+            'id'        => array('not_empty', 'message' => 'Missing identifier (short name).'),
+            'name'      => array(array('string_length', 1), 'message' => 'Name must be at least 1 characters in length, and non-empty.', 'required' => true),
+            'email'     => array('emailaddress', 'message' => 'Invalid email address provided', 'allowEmpty' => true),
+            'url'       => array(new Url(), 'message' => 'Invalid url provided', 'allowEmpty' => true),
+        );
+
+        $options = array(
+            'escapeFilter' => 'string_trim',
+        );
+
+        parent::__construct($filterRules, $validatorRules, null, $options);
+    }
+}
