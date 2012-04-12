aaee161845b8df52ca548d9d7d2c4c25d82d7194
Matthew Weier O'Phinney <matthew@zend.com>
2012-04-12 17:05:33 -0500

    Added additional metadata file used by a number of listener tests
    - Contains expected list of authors, tags, and years

---
+++ test/PhlyBlog/_posts/metadata.php
@@ -0,0 +1,42 @@
+<?php
+return array(
+    'authors' => array(
+        array(
+            'id' => 'aamilne',
+            'name' => 'A.A. Milne',
+            'email' => 'a.a@milne.com',
+            'url' => 'http://milne.com',
+        ),
+        array(
+            'id' => 'jdoe',
+            'name' => 'John Doe',
+            'email' => 'john@doe.com',
+            'url' => 'http://john.doe.com',
+        ),
+        array(
+            'id' => 'custer',
+            'name' => 'George Armstrong Custer',
+            'email' => 'me@gacuster.com',
+            'url' => 'http://www.gacuster.com',
+        ),
+        array(
+            'id' => 'crazyhorse',
+            'name' => 'Crazy Horse',
+            'email' => 'crazyhorse@siouxnation.org',
+            'url' => 'http://crazyhorse.siouxnation.org',
+        ),
+    ),
+    'tags' => array(
+        'holiday',
+        'personal',
+        'programming',
+        'thoughts',
+        'war',
+        'literature',
+        'children',
+        'draft',
+        'conferences',
+        'php',
+    ),
+    'years' => range(2004, 2011),
+);
