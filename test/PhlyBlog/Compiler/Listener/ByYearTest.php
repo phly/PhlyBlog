8ae2222fbaf433208d1dc930e7191e19573a1d04
Matthew Weier O'Phinney <matthew@zend.com>
2012-04-12 16:49:15 -0500

    Added ByYear listener tests

---
+++ test/PhlyBlog/Compiler/Listener/ByYearTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace PhlyBlog\Compiler\Listener;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+class ByYearTest extends TestCase
+{
+    public function setUp()
+    {
+        TestHelper::injectScaffolds($this);
+        $this->byYear = new ByYear($this->view, $this->writer, $this->file, $this->options);
+        $this->compiler->events()->attach($this->byYear);
+    }
+
+    public function testCreatesNoFilesPriorToCompilation()
+    {
+        $this->byYear->compile();
+        $this->assertTrue(empty($this->writer->files));
+    }
+
+    public function testCreatesFilesFollowingCompilation()
+    {
+        $this->compiler->compile();
+        $this->byYear->compile();
+
+        $this->assertFalse(empty($this->writer->files));
+
+        $filenameTemplate = $this->options->getByYearFilenameTemplate();
+        $filenameTemplate = str_replace('-p%d', '', $filenameTemplate);
+        $yearTitleTemplate = $this->options->getByYearTitle();
+        foreach ($this->expected['years'] as $year) {
+            $filename = sprintf($filenameTemplate, $year);
+            $this->assertArrayHasKey($filename, $this->writer->files);
+            $yearTitle = sprintf($yearTitleTemplate, $year);
+            $this->assertContains($yearTitle, $this->writer->files[$filename]);
+        }
+    }
+}
